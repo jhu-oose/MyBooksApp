90694ba0baebc29ec322b92ced2b9e0abed3f4aa
darvishdarab <55268581+darvishdarab@users.noreply.github.com>
2020-09-17 12:36:38 -0400

    Update DBPersistenceTest.java

--- src/test/java/DBPersistenceTest.java
+++ src/test/java/DBPersistenceTest.java
@@ -8,7 +8,7 @@ import java.sql.*;
 import static org.junit.Assert.*;
 
 
-public class TestDBPersistence {
+public class DBPersistenceTest {
 
     private static String URI;
     private static Connection conn;
